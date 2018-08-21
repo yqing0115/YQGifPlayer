c34f54f57725d536dc87fbef66c6cb177a8a1d2c
杨清 <yangqing1989@icloud.com>
2018-08-21 14:42:31 +0800

    modify intro

--- YQGifPlayer/ViewController.m
+++ YQGifPlayer/ViewController.m
@@ -3,7 +3,7 @@
 //  YQGifPlayer
 //
 //  Created by 杨清 on 2018/8/20.
-//  Copyright © 2018年 深圳市前海硕极科技有限公司. All rights reserved.
+//  Copyright © 2018年 Soargift. All rights reserved.
 //
 
 #import "ViewController.h"
